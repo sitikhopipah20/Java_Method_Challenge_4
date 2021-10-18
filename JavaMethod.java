ef2a87d96255712c35c1493af60a20517d1a135b
sitikhopipah20 <skhopipah431@gmail.com>
2021-10-18 12:35:30 +0700

    first commit

---
+++ JavaMethod.java
@@ -0,0 +1,28 @@
+public class JavaMethod {
+    public static void main(String[]args){
+        myMethod(95);
+        return;
+    }
+
+    
+    static void myMethod(int nilai) {
+  
+        if(nilai>=101){
+            System.out.println("nilai salah");
+        }  else if(nilai >=91) {
+            System.out.println("nilai:A");
+        }  else if(nilai >=76) {
+            System.out.println("nilai:B");    
+        }  else if(nilai >=51) {
+            System.out.println("nilai:C");
+        }  else if(nilai >=26) {
+            System.out.println("nilai:D"); 
+        }  else if(nilai >=0) {
+            System.out.println("nilai:E"); 
+           
+        } 
+    }  
+  
+     
+    }
+    
